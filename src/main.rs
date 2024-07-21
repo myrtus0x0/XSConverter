cd6ba7a0114dfd43749aa800b79537baf14f98df
Pim Trouerbach <ptruerbach@proofpoint.com>
2024-07-21 13:07:26 -0700

    update

--- src/main.rs
+++ src/main.rs
@@ -11,16 +11,22 @@ use nom::IResult;
 #[derive(Debug)]
 struct XsModule {
     magic: u16,
-    header_size: u16,
+    nt_magic: u16,
     section_count: u16,
     imp_key: u16,
+    hdr_size: u16,
+    unk1: u16,
     module_size: u32,
     entry_point: u32,
-    entry_point_alt: u32,
-    imports: Vec<XsImports>,
-    // exceptions,
-    // relocs,
-    // sections
+    import_header: Option<XsDataDir>,
+    exception_header: Option<XsDataDir>,
+    reloc_header: Option<XsDataDir>, // sections
+}
+
+#[derive(Debug)]
+struct XsDataDir {
+    size: u32,
+    rva: u32,
 }
 
 #[derive(Debug)]
@@ -28,54 +34,70 @@ struct XsImports {
     dll_name_rva: u32,
     first_thunk: u32,
     original_first_thunk: u32,
-    dll_length: [u8; 2],
+    dll_length: u32,
 }
 
 impl XsModule {
     fn new(module_buffer: &[u8]) -> Result<Self, String> {
         let (remaining_data, mut module) = Self::parse_header(module_buffer).unwrap();
-        let (remaining_data, imports) = Self::parse_imports(usize::from(module.section_count), remaining_data).unwrap();
 
-        module.imports = imports;
+        let (remaining_data, import_header) = Self::parse_data_table(remaining_data).unwrap();
+        module.import_header = Some(import_header);
+
+        let (remaining_data, exception_header) = Self::parse_data_table(remaining_data).unwrap();
+        module.exception_header = Some(exception_header);
+
+        let (remaining_data, reloc_header) = Self::parse_data_table(remaining_data).unwrap();
+        module.reloc_header = Some(reloc_header);
+
         Ok(module)
     }
 
     fn parse_header(module_buffer: &[u8]) -> IResult<&[u8], Self> {
         map(
-            tuple((le_u16, le_u16, le_u16, le_u16, le_u32, le_u32, le_u32)),
+            tuple((
+                le_u16, le_u16, le_u16, le_u16, le_u16, le_u16, le_u32, le_u32,
+            )),
             |(
                 magic,
-                header_size,
+                nt_magic,
                 section_count,
                 imp_key,
+                hdr_size,
+                unk1,
                 module_size,
                 entry_point,
-                entry_point_alt,
             )| XsModule {
                 magic,
-                header_size,
+                nt_magic,
                 section_count,
                 imp_key,
+                hdr_size,
+                unk1,
                 module_size,
                 entry_point,
-                entry_point_alt,
-                imports: Vec::new(),
+                import_header: None,
+                exception_header: None,
+                reloc_header: None,
             },
         )(module_buffer)
     }
 
-    fn parse_imports(section_count: usize, module_buffer: &[u8]) -> IResult<&[u8], Vec<XsImports>> {
-        many_m_n(1, section_count, Self::parse_import)(module_buffer)
+    fn parse_data_table(module_buffer: &[u8]) -> IResult<&[u8], XsDataDir> {
+        map(tuple((le_u32, le_u32)), |(size, rva)| XsDataDir {
+            size,
+            rva,
+        })(module_buffer)
     }
 
     fn parse_import(import_buffer: &[u8]) -> IResult<&[u8], XsImports> {
         map(
-            tuple((le_u32, le_u32, le_u32)),
-            |(dll_name_rva, first_thunk, original_first_thunk)| XsImports {
+            tuple((le_u32, le_u32, le_u32, le_u32)),
+            |(dll_name_rva, first_thunk, original_first_thunk, dll_length)| XsImports {
                 dll_name_rva,
                 first_thunk,
                 original_first_thunk,
-                dll_length: [0, 0]
+                dll_length,
             },
         )(import_buffer)
     }
